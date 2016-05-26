cb072255fc6e995e7d974c13d68ceadabd460523
zhouxiaotian <1282347298@qq.com>
2016-05-26 13:25:16 +0800

    20160526_全栈案例练习

---
+++ 20160526/nodeModule/customer.js
@@ -0,0 +1,131 @@
+var fs = require("fs");
+
+//->添加客户信息
+function addInfo(temp) {
+    temp = JSON.parse(temp);
+    //->先获取之前的,然后和我们的新增加的进行组合,最后把最新的重新的放入到文件中
+    var con = fs.readFileSync("./nodeModule/customerInfo.json", "utf8");
+    con = (con != "") ? JSON.parse(con) : [];
+    //->动态获取新增加数据的ID
+    if (con.length === 0) {
+        temp["id"] = 1;
+    } else {
+        temp["id"] = Number(con[con.length - 1]["id"]) + 1;
+    }
+    con.push(temp);
+    fs.writeFileSync("./nodeModule/customerInfo.json", JSON.stringify(con));
+
+    return JSON.stringify({
+        code: 0,
+        message: "创建成功~"
+    });
+}
+
+//->修改客户信息
+function updateInfo(temp) {
+    temp = JSON.parse(temp);
+    var con = fs.readFileSync("./nodeModule/customerInfo.json", "utf8");
+    con = (con != "") ? JSON.parse(con) : [];
+    var flag = false;
+    for (var i = 0, len = con.length; i < len; i++) {
+        var cur = con[i];
+        if (cur["id"] == temp["id"]) {
+            //->当前这一项就是我要修改的:循环cur中的每一项,用temp中对应的值给其进行替换
+            for (var key in cur) {
+                if (cur.hasOwnProperty(key)) {
+                    cur[key] = temp[key];
+                }
+            }
+            flag = true;
+            break;
+        }
+    }
+    var res = null;
+    if (flag) {
+        fs.writeFileSync("./nodeModule/customerInfo.json", JSON.stringify(con));
+        res = {
+            code: 0,
+            message: "修改成功~"
+        };
+    } else {
+        res = {
+            code: 1,
+            message: "修改失败,当前的客户不存在~"
+        };
+    }
+    return JSON.stringify(res);
+}
+
+//->删除客户信息
+function deleteInfo(cusId) {
+    var con = fs.readFileSync("./nodeModule/customerInfo.json", "utf8");
+    con = (con != "") ? JSON.parse(con) : [];
+    var flag = false;
+    for (var i = 0, len = con.length; i < len; i++) {
+        if (con[i]["id"] == cusId) {
+            con.splice(i, 1);
+            flag = true;
+            break;
+        }
+    }
+    //->删除成功后,我们还需要把con中的最新数据重新的写入到文件中
+    if (flag) {
+        fs.writeFileSync("./nodeModule/customerInfo.json", JSON.stringify(con));
+    }
+
+    //->创建需要返回的内容
+    var res = {
+        code: 0,
+        message: "删除成功~"
+    };
+    if (!flag) {
+        res = {
+            code: 1,
+            message: "删除失败,当前客户不存在~"
+        };
+    }
+    return JSON.stringify(res);
+}
+
+//->通过客户的编号获取对应的客户信息
+function getData(cusId) {
+    var con = fs.readFileSync("./nodeModule/customerInfo.json", "utf8");
+    con = (con != "") ? JSON.parse(con) : [];//->如果获取的内容为空,说明JSON文件中没有任何的客户信息,为了方便解析和循环,对应这种情况我们con等于一个空数组即可
+    var res = null;
+    for (var i = 0, len = con.length; i < len; i++) {
+        //->通过传递进来的客户ID,把对应的数据获取到
+        var cur = con[i];
+        if (cur["id"] == cusId) {
+            res = cur;
+            break;
+        }
+    }
+    if (!res) {//->没有获取到指定的客户信息
+        res = {
+            code: 1,
+            message: "当前客户不存在~",
+            data: null
+        };
+    } else {
+        res = {
+            code: 0,
+            message: "获取成功~",
+            data: res
+        };
+    }
+    return JSON.stringify(res);
+}
+
+//->获取所有的客户信息
+function getAllData() {
+    var con = fs.readFileSync("./nodeModule/customerInfo.json", "utf8");
+    return con;
+}
+
+module.exports = {
+    addInfo: addInfo,
+    updateInfo: updateInfo,
+    deleteInfo: deleteInfo,
+    getData: getData,
+    getAllData: getAllData
+};
---
+++ 20160526/server.js
@@ -0,0 +1,81 @@
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var suffixFn = require("./nodeModule/suffixFn");
+var customer = require("./nodeModule/customer");
+
+//->创建服务监听端口
+var server = http.createServer(function (request, response) {
+    var urlObj = url.parse(request.url, true);
+    var pathname = urlObj.pathname, query = urlObj.query;
+
+    //->客户端请求的是一些资源文件
+    var reg = /\.(HTML|JS|CSS|TXT|JSON|JPG|JPEG|PNG|GIF|BMP|ICO|SVG)/i;
+    if (reg.test(pathname)) {
+        try {
+            var suffix = reg.exec(pathname)[1].toUpperCase();
+            var suffixType = suffixFn.querySuffixType(suffix);
+            var conFile = /^(HTML|JS|CSS|TXT|JSON)$/i.test(suffix) ? fs.readFileSync("." + pathname, "utf8") : fs.readFileSync("." + pathname);
+            response.writeHead(200, {'content-type': suffixType + ";charset=utf-8;"});
+            response.end(conFile);
+        } catch (e) {
+            response.writeHead(404);
+            response.end();
+        }
+        return;
+    }
+
+    //->以下都是在实现我们的数据请求接口
+    var con = null;
+    if (pathname === "/getAllData") {
+        con = customer.getAllData();
+    }
+
+    //->GET请求传递给服务器端的信息都是存放在URL后面的问号传参中的,我们通过url.parse解析数来的query这个值中就存储了,想获取的话,直接的通过query获取即可
+    if (pathname === "/getData") {
+        var cusId = query["id"];//->获取客户端传递给服务器端的客户ID值
+        con = customer.getData(cusId);
+    }
+
+    if (pathname === "/delete") {
+        cusId = query["id"];
+        con = customer.deleteInfo(cusId);
+    }
+
+    //->POST是把内容放在HTTP的主体中传递给服务器的,不能通过URL地址解析出来了,需要我们获取主体中的内容才可以得到
+    var temp = null;
+    if (pathname === "/add") {
+        request.addListener('data', function (chunk) {
+            temp = chunk;
+        });
+        request.addListener('end', function () {
+            //->主体的内容已经接收完成
+            //temp->'{"name":"xx","age":xx,"phone":"xx","address":"xx"}'
+            temp["name"] = temp["name"] || "珠峰培训";
+            temp["age"] = temp["age"] || 25;
+            temp["phone"] = temp["name"] || "13041086186";
+            temp["address"] = temp["address"] || "北京市昌平区回龙观";
+            con = customer.addInfo(temp);
+        });
+    }
+
+    if (pathname === "/update") {
+        request.addListener('data', function (chunk) {
+            temp = chunk;
+        });
+        request.addListener('end', function () {
+            //temp->'{"id":1,"name":"xx","age":xx,"phone":"xx","address":"xx"}'
+            con = customer.updateInfo(temp);
+        });
+    }
+
+    response.writeHead(200, {'content-type': 'application/json;charset=utf-8;'});
+    response.end(con);
+
+});
+server.listen(80, function () {
+    console.log("当前服务已经启动,我们正在监听80端口~");
+});
+
+
+
